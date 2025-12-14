69f054dee3cfe0f21c11513aabf04f74d9f910ba
Mufassir Mahmud <mufassir389@gmail.com>
2025-12-15 00:39:19 +0600

    project done

--- index.js
+++ index.js
@@ -50,6 +50,26 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/equipments/:id', async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await equipmentCollections.deleteOne(query);
+      res.send(result)
+    })
+    app.put('/equipments/:id', async(req,res) =>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const options = {upsert: true};
+      const updatedEquipment = req.body
+      const updatedDoc = {
+        $set : updatedEquipment
+      }
+      const result = await equipmentCollections.updateOne(filter, updatedDoc, options);
+      res.send(result)
+    })
+    /**
+     * 
+     * */  
     app.post("/users", async(req,res) =>{
       const newUser = req.body;
       const result = await equipmentUsersCollections.insertOne(newUser);
